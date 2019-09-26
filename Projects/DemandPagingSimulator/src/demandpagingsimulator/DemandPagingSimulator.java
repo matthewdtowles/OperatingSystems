44cee1b39a6c659fd10c7189c70d8f71058548d0
matthew_towles <matthew.d.towles.ctr@mail.mil>
2019-09-26 16:25:10 -0400

    wk6 discussion check in - not done yet

---
+++ Projects/DemandPagingSimulator/src/demandpagingsimulator/DemandPagingSimulator.java
@@ -0,0 +1,27 @@
+package demandpagingsimulator;
+
+/**
+ * DemandPagingSimulator
+ * Final Project for CMSC 412 - OS
+ * Simulates the execution of:
+ * FIFO, OPT, LRU, & LFU
+ * for a computer with N frames [N lt 8]
+ * N provided by user as argument
+ * Assumes single process running has 10 frames
+ * Algorithms sim'd based on ref string 
+ * Ref string read from keyboard or random
+ * 
+ * @author matthew.towles
+ * @date Sep 26, 2019
+ */
+public class DemandPagingSimulator {
+
+    
+    /**
+     * Main
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+    }
+}
